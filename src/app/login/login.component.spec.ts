61f0a568e52c813c51760fc46eba81d38d246a65
unknown <f.salem@pharmainvest.dz>
2019-03-12 14:07:15 +0100

    new tests

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -27,5 +28,22 @@ describe('LoginComponent', () => {
     component.userLogin.get('username').setValue('fairouz');
     component.userLogin.get('password').setValue('fairouz05');
     expect(component.userLogin.valid).toBe(true);
+  });
+  it('should have invalid form ', () => {
+    component.userLogin.reset();
+    expect(component.userLogin.valid).toBe(false);
+  });
+  it('should have valid username and password', () => {
+    component.userLogin.get('username').setValue('fairouz');
+    component.userLogin.get('password').setValue('fairouz05');
+    expect(component.userLogin.get('username').valid).toBe(true);
+    expect(component.userLogin.get('password').valid).toBe(true);
+  });
+  it('should simulat button click', () => {
+    spyOn(component, 'login');
+    const btnDg = fixture.debugElement.query(By.css('#btn')).nativeElement;
+    btnDg.click();
+    component.login();
+    expect(component.login).toHaveBeenCalledTimes(1);
   });
 });
