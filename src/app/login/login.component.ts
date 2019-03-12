90ffd78fa61b799ea90477b63474bff14c1d7455
unknown <n.karce@pharmainvest.dz>
2019-03-12 10:17:38 +0100

    modified login template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  constructor(private _FB: FormBuilder) {}
 
-  constructor() { }
+  userLogin = this._FB.group({
+    username: this._FB.control('', [
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    password: this._FB.control('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  });
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
